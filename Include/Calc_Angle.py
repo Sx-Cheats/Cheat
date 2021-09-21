d1e23d811ff9b67921810d1a55c6c017a58ba73b
Sx-Cheats <65744142+Sx-Cheats@users.noreply.github.com>
2021-09-21 19:12:34 +0200

    Update Calc_Angle.py

--- Include/Calc_Angle.py
+++ Include/Calc_Angle.py
@@ -7,7 +7,7 @@ class Calc():
         self.origine = tuple(map(abs,[x[0]-x[1] for x in zip(_TARGET,_PLAYER)]))
         self.hypotenuse = sqrt(self.origine[0]**2+self.origine[2]**2)
         self.X = acos(self.origine[0]/self.hypotenuse)*180/pi
-        self.Y =  sin(self.origine[1]/sqrt(self.hypotenuse**2+self.origine[0]))*180/pi
+        self.Y =  sin(self.origine[1]/sqrt(self.hypotenuse**2+self.origine[0]**2))*180/pi
         if  _PLAYER[2] <= _TARGET[2]:
                 if _PLAYER[0] <= _TARGET[0]:
                  self.X =  180 - self.X
