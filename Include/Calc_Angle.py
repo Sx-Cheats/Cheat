35dcf98211b8bbdd44f47eac6808b20d08aff2f2
Sx-Cheats <65744142+Sx-Cheats@users.noreply.github.com>
2021-09-21 17:17:30 +0200

    Create Calc_Angle.py

---
+++ Include/Calc_Angle.py
@@ -0,0 +1,24 @@
+
+from math import cos,acos,sin,asin,tan,atan,sqrt,pi
+class Calc():
+    
+    def __init__(self) -> None:
+        pass 
+    def ANGLE(self,_PLAYER:tuple=(0,0,0),_TARGET:tuple=(0,0,0)):
+        self.origine = tuple(map(abs,[x[0]-x[1] for x in zip(_TARGET,_PLAYER)]))
+        self.hypotenuse = sqrt(self.origine[0]**2+self.origine[2]**2)
+        self.X = acos(self.origine[0]/self.hypotenuse)*180/pi
+        self.Y =  sin(self.origine[1]/sqrt(self.hypotenuse**2+self.origine[0]))*180/pi
+        if  _PLAYER[2] <= _TARGET[2]:
+                if _PLAYER[0] <= _TARGET[0]:
+                 self.X =  180 - self.X
+        elif _PLAYER[2] > _TARGET[2]:
+             if _PLAYER[0] >= _TARGET[0]:
+               self.X = 360 - self.X
+             elif  _PLAYER[0] < _TARGET[0]:
+                 self.X += 180
+        if _PLAYER[1] > _TARGET[1]:
+            self.Y = -self.Y
+        elif _PLAYER[1] < _TARGET[1] and self.Y <  0:
+          self.Y = abs(self.Y)
+        return  self.X,self.Y
